14c7cac0f6eb1ec927445cdfc31c460d8760b479
MontesR033 <rafaelmontes691@gmail.com>
2022-08-24 21:44:24 -0300

    conexion de front end con back end e implementacion de login

---
+++ back end/mr/src/main/java/com/portfolio/mr/Repository/IPersonaRepository.java
@@ -0,0 +1,10 @@
+package com.portfolio.MR.Repository;
+
+import com.portfolio.MR.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona,Long> {
+
+}
